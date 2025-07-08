9859f0f2050252d401a50a29d196790103ec1ff6
gokul-flutter-dev <gokulakrishnan.g@vim.zone>
2025-07-08 11:05:05 +0530

    update version 2.4

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ sendBtn.addEventListener("click", async function () {
     output.innerText = "Error: " + e;
   }
 });
+
+playVideo.addEventListener("click",async function(){
+ try {
+      const response = await webf.methodChannel.invokeMethod("playVideo", [
+        "https://videos.pexels.com/video-files/28952496/12526868_1920_1080_30fps.mp4"
+      ]);
+      output.textContent = "Video playing: " + response;
+    } catch (e) {
+      output.innerText = "Error: " + e;
+    }
+});
